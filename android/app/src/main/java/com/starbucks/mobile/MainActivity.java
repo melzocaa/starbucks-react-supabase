7af1e5ab866399f7cc1d11e997b32c676d07042f
Melanie Martins <melanie.martins@aluno.senai.br>
2026-09-25 09:04:36 -0300

    Adiciona workflow para gerar APK

---
+++ android/app/src/main/java/com/starbucks/mobile/MainActivity.java
@@ -0,0 +1,5 @@
+package com.starbucks.mobile;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++
@@ -1,3 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-const config: CapacitorConfig={appId:'com.starbucks.mobile',appName:'Starbucks',webDir:'dist'};
-export default config;
